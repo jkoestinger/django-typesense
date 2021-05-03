505c643bb821a6fa4ba8b180c70bde7dae3cf238
Justin Köstinger <jkoestinger@gmail.com>
2021-05-03 22:30:18 +0200

    Added base packaging tools

---
+++ run-tests.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+django-admin.py test --settings=test_settings
---
+++ test_settings.py
@@ -0,0 +1,10 @@
+INSTALLED_APPS = (
+    'typesense',
+)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+SECRET_KEY = "testing-secret-key"
